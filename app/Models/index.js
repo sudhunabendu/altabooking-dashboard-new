cbb480c0d92f2a8cfbb86a3d2aeb5519ea80c090
sudhunabendu <nabendubose1991@gmail.com>
2024-06-23 02:02:58 +0530

    update1

---
+++ app/Models/index.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const db = {};
+
+db.mongoose = mongoose;
+
+// db.ab_currencies = require("./currency.model");
+// db.ab_user_details = require("./userdetail.model");
+// db.ab_user_codes = require("./usercode.model");
+// db.ab_user = require("./user.model");
+// db.ab_email_template = require("./emailTemplate.model");
+// db.ab_roles= require("./roles.model");
+
+module.exports = db;
--- server.js
+++ server.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
+const dotenv = require('dotenv').config()
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const path = require("path");
 const port = process.env.PORT || 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+var corsOptions = {
+    origin: "http://localhost:3000"
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); // parse form data client
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(fileUpload());
+
+// Models
+const db = require("./app/Models");
+
+// db.mongoose.connect(`mongodb+srv://NodeClass:Goodtime2323%40@cluster0.fwvqxqi.mongodb.net/altabookingTest`, {
+db.mongoose.connect(`mongodb://127.0.0.1:27017/altabooking-dashboard`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Successfully connect to MongoDB.");
+    // initial();
+}).catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+});
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to NodeJS with mongoose application." });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port http://localhost:${port}`);
 });
